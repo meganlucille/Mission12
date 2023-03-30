cf4a4fed17466a38cfa87ec715664db8d815146c
meganlucille <megan@mightyjohnsons.com>
2023-03-30 10:08:08 -0600

    Mission12

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import TopBanner from './Nav';
+import Home from './Home';
+import Movies from './Movies';
+import Podcasts from './Podcasts';
 
-function App() {
+function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TopBanner />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/podcasts" element={<Podcasts />} />
+      </Routes>
+      <br></br>
+    </>
   );
 }
 
